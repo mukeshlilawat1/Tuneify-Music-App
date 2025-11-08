f0596b6da657905b683018cb06ff5398769b7ed1
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-11-09 00:19:19 +0530

    - Implemented UserRepository interface extending MongoRepository<User, String> - Added custom methods:   • findByEmail(String email) → to fetch user by email   • existsByEmail(String email) → to check email existence during registration - Supports authentication and validation in AuthController

---
+++ com.Tuneify-Music-App/src/main/java/com/Tuneify_Music_App/Document/User.java
@@ -0,0 +1,29 @@
+package com.Tuneify_Music_App.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class User {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+    private Role role = Role.USER;
+    private enum Role {
+        USER,
+        ADMIN
+    }
+}
---
+++ com.Tuneify-Music-App/src/main/java/com/Tuneify_Music_App/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Tuneify_Music_App.Repository;
+
+import com.Tuneify_Music_App.Document.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
